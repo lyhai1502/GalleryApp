6c1c062ec2838fd939da2e1b8b5364b4512e9aab
MinhSkyDev <minhdev2002@gmail.com>
2022-11-26 08:32:33 +0700

    Redesign Album layout

--- app/src/main/java/com/example/galleryapp/Fragment/Album/AlbumAdapter.java
+++ app/src/main/java/com/example/galleryapp/Fragment/Album/AlbumAdapter.java
@@ -2,6 +2,7 @@ package com.example.galleryapp.Fragment.Album;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,17 +13,22 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.galleryapp.R;
+import com.makeramen.roundedimageview.RoundedImageView;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Random;
 
 //Class này sẽ làm Adapter cho từng thành phần Albụm
 public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder> {
 
     private ArrayList<String> names;
+    private int[] bg_names;
 
-    public AlbumAdapter(ArrayList<String> names){
+    public AlbumAdapter(ArrayList<String> names, int[] bg_names)
+    {
         this.names = names;
+        this.bg_names = bg_names;
     }
 
     @NonNull
@@ -32,10 +38,20 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
         return new AlbumViewHolder(recyclerViewAlbum);
     }
 
+
+    private int getRandomIndex(){
+        int index = 0;
+        int size_bg = bg_names.length;
+        Random dice = new Random();
+        index = dice.nextInt(size_bg);
+        return index;
+    }
+
     @Override
     public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
-
         holder.album_textView.setText(names.get(position));
+        int randomIndex = this.getRandomIndex();
+        holder.album_imgView.setImageResource(bg_names[randomIndex]);
 
     }
 
@@ -48,12 +64,13 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
 
         //public ImageView album_imgView;
         public TextView album_textView;
+        public RoundedImageView album_imgView;
 
         public AlbumViewHolder(@NonNull View itemView) {
             super(itemView);
             //album_imgView = itemView.findViewById(R.id.album_imgView);
             album_textView = itemView.findViewById(R.id.album_textView);
-
+            album_imgView = itemView.findViewById(R.id.album_imgView);
         }
     }
 }
--- app/src/main/java/com/example/galleryapp/Fragment/AlbumFragment.java
+++ app/src/main/java/com/example/galleryapp/Fragment/AlbumFragment.java
@@ -20,17 +20,30 @@ public class AlbumFragment extends Fragment {
 
     private RecyclerView album_recyclerview;
     private AlbumAdapter adapter;
+
+    private int[] bg = {
+            R.drawable.bg1,
+            R.drawable.bg2,
+            R.drawable.bg3,
+            R.drawable.bg4,
+            R.drawable.bg5,
+            R.drawable.bg6,
+            R.drawable.bg7,
+            R.drawable.bg8,
+            R.drawable.bg9,
+    };
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_album, container, false);
 
         album_recyclerview = view.findViewById(R.id.album_recyclerview);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(),2);
-        album_recyclerview.setLayoutManager(gridLayoutManager);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        album_recyclerview.setLayoutManager(layoutManager);
 
         MainActivity mainActivity = (MainActivity) getActivity();
-        adapter = new AlbumAdapter(mainActivity.albumNames);
+        adapter = new AlbumAdapter(mainActivity.albumNames, bg);
 
         album_recyclerview.setAdapter(adapter);
         return view;
